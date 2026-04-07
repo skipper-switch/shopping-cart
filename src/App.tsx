a851275dc7d5963560a2eb5a8a25442e2083f303
skipper-switch <oluwatosin.adegoroye.interswitchng.com>
2026-04-07 16:13:51 +0100

    feat: status added

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@
 import { Routes, Route } from "react-router-dom"
 import { Container } from "react-bootstrap"
 import { Home } from "./pages/Home"
-// import { Store } from "./pages/Store"
+import { Store } from "./pages/Store"
 import { About } from "./pages/About"
-import Navbar from "./components/Navbar"
-// import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+import { Navbar } from "./components/Navbar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+
 
 
 
@@ -13,18 +14,16 @@ function App() {
   
 
   return (
-    // <ShoppingCartProvider>
-    <>
+    <ShoppingCartProvider>
      <Navbar />
       <Container className="mb-4">
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/store" element={<Store />} /> */}
+          <Route path="/store" element={<Store />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </Container>
-    </>
-    // </ShoppingCartProvider>
+    </ShoppingCartProvider>
   )
 }
 
